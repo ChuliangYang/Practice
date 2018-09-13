2e68bcefa703a2dd86e7c2983067851499726659
CarlosYang <mrcarlosyang@gmail.com>
2018-09-13 18:33:37 -0400

    dateapp

--- Datingapp/app/src/main/java/com/me/cl/datingapp/Adapter.kt
+++ Datingapp/app/src/main/java/com/me/cl/datingapp/Adapter.kt
@@ -3,25 +3,32 @@ package com.me.cl.datingapp
 import android.databinding.DataBindingUtil
 import android.databinding.ViewDataBinding
 import android.support.v7.widget.RecyclerView
+import android.view.LayoutInflater
 import android.view.ViewGroup
+import com.bumptech.glide.Glide
+import com.me.cl.datingapp.databinding.ItemDateBinding
+import kotlinx.android.synthetic.main.item_date.view.*
 
 /**
  * Created by CL on 9/12/18.
  */
-class Adapter(var dataList:List<Date.DataBean>):RecyclerView.Adapter<BindingViewHolder>() {
+class Adapter(var dataList:List<Date.DataBean>):RecyclerView.Adapter<BindingViewHolder<ItemDateBinding>>() {
 
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): BindingViewHolder {
-        return DataBindingUtil.inflate<>(parent.context,R.layout.item_date,parent,false)
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): BindingViewHolder<ItemDateBinding> {
+        return BindingViewHolder(DataBindingUtil.inflate(LayoutInflater.from(parent.context),R.layout.item_date,parent,false))
     }
 
     override fun getItemCount(): Int {
         return dataList.size
     }
 
-    override fun onBindViewHolder(holder: BindingViewHolder, position: Int) {
-        holder.
+    override fun onBindViewHolder(holder: BindingViewHolder<ItemDateBinding>, position: Int) {
+        holder.viewDataBinding.dataBean=dataList[position]
+        holder.viewDataBinding.root.apply {
+            Glide.with(context).load(dataList[position].photo.thumb_paths).into(iv_photo)
+        }
     }
 }
 
-class BindingViewHolder(var viewDataBinding: ViewDataBinding):RecyclerView.ViewHolder(viewDataBinding.root)
+class BindingViewHolder<T:ViewDataBinding>(var viewDataBinding: T):RecyclerView.ViewHolder(viewDataBinding.root)
--- Datingapp/app/src/main/java/com/me/cl/datingapp/MainActivity.kt
+++ Datingapp/app/src/main/java/com/me/cl/datingapp/MainActivity.kt
@@ -2,8 +2,10 @@ package com.me.cl.datingapp
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.support.v7.widget.GridLayoutManager
 import io.reactivex.android.schedulers.AndroidSchedulers
 import io.reactivex.schedulers.Schedulers
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -12,7 +14,8 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         RetrofitFactory.buildRetrofit().create(DateService::class.java).apply {
             getDate().subscribeOn(Schedulers.io()).observeOn(AndroidSchedulers.mainThread()).subscribe {
-
+                rv_date.layoutManager=GridLayoutManager(this@MainActivity,2)
+                rv_date.adapter=Adapter(it.data)
             }
         }
     }
